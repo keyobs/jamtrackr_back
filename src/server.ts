ded0db9851cbb8d463eaf69d6b2b3866e9e6414e
keyobs <elodiekoob@gmail.com>
2024-02-13 14:43:22 +0100

    config: husky/lint staged + pre-commit

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import { Request, Response } from "express";
-import dotenv from "dotenv";
-import app from "../index";
+import { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import app from '../index';
 
 dotenv.config();
 
 const port = process.env.PORT || 9001;
 
-app.get("/api", (req: Request, res: Response) => {
-  res.send("you summouned me !");
+app.get('/api', (req: Request, res: Response) => {
+    res.send('you summouned me !');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
