feb61a7bb42a30d5e92563860ef058cd3ad9dc45
keyobs <elodiekoob@gmail.com>
2024-02-20 12:42:46 +0100

    db: connect

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import { query } from './config/db';
 
 const app: Express = express();
 
@@ -10,6 +11,16 @@ app.use(cors());
 app.get('/api', (req: Request, res: Response) => {
     const response = res.status(200);
     response.send('you summouned me !');
+});
+
+app.get('/v1', async (req, res) => {
+    try {
+        const result = await query('SELECT * FROM leagues', []); // Use the imported query function
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error oh no');
+    }
 });
 
 export default app;
---
+++ src/config/db.ts
@@ -0,0 +1,14 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
+
+export const query = (text, params) => pool.query(text, params ? params : []);
